b461ac07860b5790fc0a3a68617cf2cadab38d95
unknown <wilfriedngassarou23@gmail.com>
2025-05-05 20:00:59 +0200

    Refactor: replace answer_id and answer_ids by answer_ids

--- src/utils/get-default-selection.ts
+++ src/utils/get-default-selection.ts
@@ -2,7 +2,6 @@ import { Proposition, Question } from "../types"
 
 type Param = (Question & { propositions: Proposition[] })[]
 
-// export const getDefaultSelection = (questions: Param) => {
 export const getDefaultSelection = (questions: Param) => questions.map(q => ({
   question_id: q.id,
   answer_ids: [] as number[]
